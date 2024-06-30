769398b18f06e03f0906866e7859040a6c235603
Tanguy Dumontier <tangdumontier76@gmail.com>
2024-06-30 18:49:16 +0200

    feat(daimc.py): add DAIMC and UpdateV_DAIMC method, translated from matlab codes

--- imvc/cluster/daimc.py
+++ imvc/cluster/daimc.py
@@ -1,9 +1,12 @@
+import logging
 import os
 from os.path import dirname
 
 import numpy as np
 import oct2py
 import pandas as pd
+
+from control.matlab import lyap
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.cluster import KMeans
 
@@ -101,15 +104,7 @@ class DAIMC(BaseEstimator, ClassifierMixin):
         -------
         self :  Fitted estimator.
         """
-        Xs = check_Xs(Xs, force_all_finite='allow-nan')
-        if isinstance(Xs[0], pd.DataFrame):
-            transformed_Xs = [X.values for X in Xs]
-        elif isinstance(Xs[0], np.ndarray):
-            transformed_Xs = Xs
-        observed_view_indicator = get_observed_view_indicator(transformed_Xs)
-        transformed_Xs = simple_view_imputer(transformed_Xs, value="zeros")
-        transformed_Xs = [X.T for X in transformed_Xs]
-        transformed_Xs = tuple(transformed_Xs)
+        Xs = check_Xs(Xs, force_all_finite='allow-nan', copy=True)
 
         if self.engine == "matlab":
             matlab_folder = dirname(__file__)
@@ -123,21 +118,40 @@ class DAIMC(BaseEstimator, ClassifierMixin):
             oc.eval("pkg load control")
             oc.warning("off", "Octave:possible-matlab-short-circuit-operator")
 
+            if isinstance(Xs[0], pd.DataFrame):
+                transformed_Xs = [X.values for X in Xs]
+            elif isinstance(Xs[0], np.ndarray):
+                transformed_Xs = Xs
+            observed_view_indicator = get_observed_view_indicator(transformed_Xs)
+            transformed_Xs = simple_view_imputer(transformed_Xs, value="zeros")
+            transformed_Xs = [X.T for X in transformed_Xs]
+            transformed_Xs = tuple(transformed_Xs)
+
             w = tuple([oc.diag(missing_view) for missing_view in observed_view_indicator.T])
             if self.random_state is not None:
-                oc.rand('seed', self.random_state)
+                oc.rand('twister', self.random_state)
+
             u_0, v_0, b_0 = oc.newinit(transformed_Xs, w, self.n_clusters, len(transformed_Xs), nout=3)
-            return u_0, v_0, b_0
-            # u, v, b, f, p, n = oc.DAIMC(transformed_Xs, w, u_0, v_0, b_0, None, self.n_clusters,
-            #                             len(transformed_Xs), {"afa": self.alpha, "beta": self.beta}, nout=6)
+            u, v, b, f, p, n = oc.DAIMC(transformed_Xs, w, u_0, v_0, b_0, None, self.n_clusters,
+                                        len(transformed_Xs), {"afa": self.alpha, "beta": self.beta}, nout=6)
         elif self.engine == "python":
             # Here the code which is the strict translation from MATLAB
 
+            if isinstance(Xs[0], pd.DataFrame):
+                transformed_Xs = [X.values for X in Xs]
+            elif isinstance(Xs[0], np.ndarray):
+                transformed_Xs = Xs
+            observed_view_indicator = get_observed_view_indicator(transformed_Xs)
+            transformed_Xs = simple_view_imputer(transformed_Xs, value="zeros")
+            transformed_Xs = [X.T for X in transformed_Xs]
+            transformed_Xs = tuple(transformed_Xs)
+
             w = tuple([np.diag(missing_view) for missing_view in observed_view_indicator.T])
             u_0, v_0, b_0 = self._newInit(transformed_Xs, w, self.n_clusters, len(transformed_Xs))
-            return u_0, v_0, b_0
+            u, v, b, f, p, n = self._DAIMC(transformed_Xs, w, u_0, v_0, b_0, None, self.n_clusters,
+                                           len(transformed_Xs), {"afa": self.alpha, "beta": self.beta})
         else:
-            raise ValueError("Only engine=='matlab' is currently supported.")
+            raise ValueError("Only engine=='matlab' and 'python' are currently supported.")
 
         model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
         self.labels_ = model.fit_predict(X=v)
@@ -186,7 +200,6 @@ class DAIMC(BaseEstimator, ClassifierMixin):
         labels = self.fit(Xs)._predict(Xs)
         return labels
 
-    # Example function where random state is set for KMeans
     def _newInit(self, X, W, r, viewNum, random_state=42):
         np.random.seed(random_state)
 
@@ -197,11 +210,11 @@ class DAIMC(BaseEstimator, ClassifierMixin):
 
         for i in range(viewNum):
             item = np.diag(W[i])
-            temp = np.where(item == 0)[0]
+            temp = np.where(item == 0)
             XX[i] = X[i].copy()
             XX[i] = np.delete(XX[i], temp, axis=1)
             Mx = np.mean(XX[i], axis=1, keepdims=True)
-            X[i][:, temp] = np.tile(Mx, (1, len(temp)))
+            X[i][:, temp[0]] = np.tile(Mx, (1, len(temp)))
 
         sumH = 0
         for i in range(viewNum):
@@ -209,7 +222,7 @@ class DAIMC(BaseEstimator, ClassifierMixin):
             kmeans = KMeans(n_clusters=r, n_init=20, random_state=random_state)
             ilabels = kmeans.fit_predict(X[i].T)
             C = kmeans.cluster_centers_
-            U[i] = C.T + 0.1 * np.ones((d, r))
+            U[i] = C.T + (0.1 * np.ones((d, r)))
             G = np.zeros((n, r))
             for j in range(r):
                 G[:, j] = (ilabels == j).astype(float)
@@ -232,3 +245,87 @@ class DAIMC(BaseEstimator, ClassifierMixin):
 
         return U, V, B
 
+    def _DAIMC(self, X, W, U, V, B, label, r, viewNum, options):
+        eta = 1e-10
+        F = 0
+        P = 0
+        N = 0
+        D = [np.zeros((B[i].shape[0], B[i].shape[0])) for i in range(viewNum)]
+
+        for i in range(viewNum):
+            for k in range(B[i].shape[0]):
+                D[i][k, k] = 1 / np.sqrt(np.linalg.norm(B[i][k, :], 2) ** 2 + eta)
+
+        time = 0
+        f = 0
+        while True:
+            time = time + 1
+            for i in range(viewNum):
+                tmp1 = options['afa'] * B[i] @ B[i].T
+                tmp2 = V.T @ W[i] @ V
+                tmp3 = X[i] @ W[i] @ V + options['afa'] * B[i]
+                U[i] = lyap(tmp1, tmp2, -tmp3)
+
+            V = self._UpdateV_DAIMC(X, W, U, V, viewNum)
+            Q = np.diag(np.ones(V.shape[0], ) @ V)
+            V = V @ np.linalg.inv(Q)
+            for i in range(viewNum):
+                U[i] = U[i] @ Q
+                invD = np.diag(1. / np.diag(0.5 * options['beta'] * D[i]))
+                B[i] = (invD - invD @ U[i] @ np.linalg.inv(U[i].T @ invD @ U[i] + np.eye(r)) @ U[i].T @ invD) @ U[i]
+                for k in range(B[i].shape[0]):
+                    D[i][k, k] = 1 / np.sqrt(np.linalg.norm(B[i][k, :], 2) ** 2 + eta)
+
+            ff = 0
+            for i in range(viewNum):
+                tmp1 = (X[i] - U[i] @ V.T) @ W[i]
+                tmp2 = B[i].T @ U[i] - np.eye(r)
+                tmp3 = np.sum(1. / np.diag(D[i]))
+                ff = ff + np.sum(np.sum(tmp1 ** 2)) + options['afa'] * np.sum(np.sum(tmp2 ** 2)) + options[
+                    'beta'] * tmp3
+
+            F += ff
+            if (np.abs(ff - f) / f < 1e-4) or (np.abs(ff - f) > 1e100) | (time == 30):
+                break
+            f = ff
+        return U, V, B, F, P, N
+
+    def _UpdateV_DAIMC(self, X, W, U, V, viewNum):
+        time = 0
+        f = 0
+        while True:
+            time = time + 1
+            sumVUUminus = 0
+            sumVUUplus = 0
+            sumXUminus = 0
+            sumXUplus = 0
+
+            for i in range(viewNum):
+                XU = X[i].T @ U[i]
+                absXU = np.abs(XU)
+                XUplus = (absXU + XU) / 2
+                XUminus = (absXU - XU) / 2
+
+                UU = U[i].T @ U[i]
+                absUU = np.abs(UU)
+                UUplus = (absUU + UU) / 2
+                UUminus = (absUU - UU) / 2
+
+                sumXUminus = sumXUminus + W[i] @ XUminus
+                sumXUplus = sumXUplus + W[i] @ XUplus
+
+                sumVUUplus = sumVUUplus + W[i] @ V @ UUplus
+                sumVUUminus = sumVUUminus + W[i] @ V @ UUminus
+
+            V = V * np.sqrt((sumXUplus + sumVUUminus) / (np.maximum(sumXUminus + sumVUUplus, 1e-10)))
+            ff = 0
+
+            for i in range(viewNum):
+                tmp = (X[i] - U[i] @ V.T) @ W[i]
+                ff = ff + np.sum(np.sum(tmp ** 2))
+
+            if (np.abs((ff - f) / f) < 1e-4) | (np.abs(ff - f) > 1e100) | (time == 30):
+                break
+
+            f = ff
+        return V
